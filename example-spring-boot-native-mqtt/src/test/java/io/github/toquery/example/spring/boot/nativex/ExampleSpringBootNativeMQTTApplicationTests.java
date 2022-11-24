06955445f752880dc9222a7af1ff185e55ad2523
ToQuery <toquery@qq.com>
2022-11-24 22:36:31 +0800

    init

--- example-spring-boot-native-mqtt/src/test/java/io/github/toquery/example/spring/boot/nativex/ExampleSpringBootNativeMQTTApplicationTests.java
+++ example-spring-boot-native-mqtt/src/test/java/io/github/toquery/example/spring/boot/nativex/ExampleSpringBootNativeMQTTApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ExampleSpringBootNativeApplicationTests {
+class ExampleSpringBootNativeMQTTApplicationTests {
 
 	@Test
 	void contextLoads() {
