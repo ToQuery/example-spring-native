857dbc0ad8d3892463a791a49e7cc864f4686e80
ToQuery <toquery@qq.com>
2022-11-24 19:26:03 +0800

    init

---
+++ example-spring-boot-native-demo/src/main/java/io/github/toquery/ExampleSpringBootNativeApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringBootNativeApplication.class, args);
+	}
+
+}
---
+++ example-spring-boot-native-demo/src/test/java/io/github/toquery/ExampleSpringBootNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringBootNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
